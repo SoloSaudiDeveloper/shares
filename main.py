72ae3f6c886efa8b14aef5d37f9dd8d77801f8a8
SoloSaudiDeveloper <129058502+SoloSaudiDeveloper@users.noreply.github.com>
2023-12-25 21:08:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def process_element(element_html, number, output_data):
 # Initialize Selenium WebDriver
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
-#chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument("--disable-gpu")
 browser = webdriver.Chrome(options=chrome_options)
