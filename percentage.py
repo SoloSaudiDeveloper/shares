d0d701031c4ca801cc4fcbde28f74f8542b1b615
SoloSaudiDeveloper <SoloSaudiDeveloper@gmail.com>
2024-01-20 08:23:09 +0300

    Update percentage.py

--- percentage.py
+++ percentage.py
@@ -112,7 +112,7 @@ xpaths_list = [
         '//*[@id="js-category-content"]/div[2]/div/div/div[5]/div[2]/div/div[1]/div[4]/div[5]/div[6]',
         '//*[@id="js-category-content"]/div[2]/div/div/div[5]/div[2]/div/div[1]/div[4]/div[5]/div[7]',
 
-      //*[@id="js-category-content"]/div[2]/div/div/div[5]/div[2]/div/div[1]/div[4]/div[5]/div[1]
+      
         
         # ... other XPaths for the fourth row
     ]
